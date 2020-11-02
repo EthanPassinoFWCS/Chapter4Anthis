f5d6ff3cdd2bcb8aeafc405fdba8f1ce0dc3c5be
EthanPassinoFWCS <69813689+EthanPassinoFWCS@users.noreply.github.com>
2020-11-02 15:17:02 -0500

    Add files via upload

---
+++ TIY4_1.py
@@ -0,0 +1,7 @@
+pizzas = ["cheesepizza", "pepperonipizza", "sasagepizza"]
+
+for pizza in pizzas:
+  print(f"I like {pizza}")
+
+print("I really love pizza!")
+
---
+++ TIY4_2.py
@@ -0,0 +1,6 @@
+animals = ["cat", "dog", "monkey"]
+
+for animal in animals:
+  print(f"A {animal} would be a great pet.")
+
+print("All of these animals have tails!")
